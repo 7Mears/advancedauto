89d85cd05ff08f526c762d1bfc3c2dc306dde97e
7Mears <7mears@gmail.com>
2015-04-13 13:01:42 -0600

    trying to organize colors

--- themes/advancedauto/footer.php
+++ themes/advancedauto/footer.php
@@ -8,16 +8,13 @@
  */
 ?>
 
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'advancedauto' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'advancedauto' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'advancedauto' ), 'Advanced Auto', '<a href="http://www.vireoproductions.com" rel="designer">Vireo</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+<footer id="colophon" class="site-footer" role="contentinfo">
+	<div class="site-info">
+		<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'advancedauto' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'advancedauto' ), 'WordPress' ); ?></a>
+		<span class="sep"> | </span>
+		<?php printf( __( 'Theme: %1$s by %2$s.', 'advancedauto' ), 'Advanced Auto', '<a href="http://www.vireoproductions.com" rel="designer">Vireo</a>' ); ?>
+	</div><!-- .site-info -->
+</footer><!-- #colophon -->
 
 <?php wp_footer(); ?>
 
